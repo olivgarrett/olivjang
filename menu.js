997702f28d98caa87f6fa70c0a4ca34713896ec1
Olivia Garrett <ojg5g8@mail.missouri.edu>
2019-09-09 09:29:57 -0500

    notes

--- menu.js
+++ menu.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   $('.menu-trigger').click(function() {
     $('nav ul').slideToggle(500);
-  });//end slide toggle
+  });
 
 $(window).resize(function() {
 	 if (  $(window).width() > 700 ) {
 		$('nav ul').removeAttr('style');
 	 }
-	});//end resize
-});//end ready
+	});
+});
